0c699326a6d4883cd1d4206e997f1fbefa782464
CodeHunter2703 <shivamcode2703@gmail.com>
2025-04-22 22:41:24 +0530

    Update and rename lab 4_prime.py to Lab4/lab 4_prime.py
    moving file to a folder

--- Lab4/lab 4_prime.py
+++ Lab4/lab 4_prime.py
@@ -1,6 +1,6 @@
 n=int(input('enter a number : '))
 if n>1:
-    for i in range(2, n):
+    for i in range(2, n/2):
         if (n%i) == 0:
             print('the number is not prime')
         else:
